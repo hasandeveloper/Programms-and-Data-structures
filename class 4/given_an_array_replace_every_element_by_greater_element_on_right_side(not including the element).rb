4a423f85db41408867230b432bca329dacb820e6
hasan <hasan.ali@kloctechnologies.co.in>
2020-11-28 10:31:58 +0530

    code: given_an_array_replace_every_element_by_greater_element_on_right_side(not including the element)

---
+++ class 4/given_an_array_replace_every_element_by_greater_element_on_right_side(not including the element).rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+def right_hand_side_greater_element(array)
+    right_hand_elements = []
+    highest_right_hand_elements = []
+
+    index=0
+      array.each_with_index do |v, i|
+
+        if index != i
+            #push right hand side elements
+            length = array.length
+            number = length-i
+            number.times do |n|
+                right_hand_elements.push(array[n+i])
+            end
+
+            #swap the elements to highest 
+            right_hand_elements.each_with_index do |value, index|
+            if right_hand_elements[index+1] != nil
+                if value > right_hand_elements[index+1]
+                    right_hand_elements[index], right_hand_elements[index+1] = right_hand_elements[index+1], right_hand_elements[index]
+                
+                end
+            end
+            end
+
+            #push the highest swaped element
+            highest_right_hand_elements.push(right_hand_elements.last)
+
+
+            right_hand_elements.clear()
+
+        end
+
+      end
+    highest_right_hand_elements.push(-1)
+end
+
+p right_hand_side_greater_element([16,17,4,3,5,2])
