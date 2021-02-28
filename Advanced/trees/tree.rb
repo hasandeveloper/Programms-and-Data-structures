f99b11ede35a77d682fcc7d6b13d7ef55411c583
hasan <hasan.ali@kloctechnologies.co.in>
2021-02-28 11:54:07 +0530

    feat: binary tree

---
+++ Advanced/trees/tree.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+class Node
+    attr_reader :value
+    attr_accessor :left, :right
+
+    def initialize(value)
+     @value = value
+     @left =nil
+     @right =nil
+    end
+
+end
+
+
+
+def pushNode(node, value)
+
+    if(value > node.value)
+        
+        if(node.right)
+            
+        pushNode(node.right, value)
+
+        else
+            
+        node.right = Node.new(value)
+
+        end
+
+    else
+
+        if(node.left)
+            
+        pushNode(node.left, value)
+
+        else
+            
+        node.left = Node.new(value)
+
+        end
+        
+    end
+
+end
+
+arr = [5,6,2,4,1,8,7,9,3];
+root = Node.new(arr.shift);
+arr.each{|e| pushNode(root, e) }
