715595c3c1ba43cb6358145763d07fc516e8b8da
hasan <hasan.ali@kloctechnologies.co.in>
2021-03-03 15:24:05 +0530

    feat: recursion class complete

--- Advanced/recursion/fibonacci.rb
+++ Advanced/recursion/fibonacci.rb
@@ -1,11 +1,52 @@
-# calculating the Nth Fibonacci number. Here the base case would be the when the N is less than 2, as then the Fibonacci number would be itself. The recursive call would be calling the method for calculating the (n-1)th and (n-2)nd Fibonacci number and adding it to get the result for Nth number.
-def fibonacci(n)
-    if number < 2
-        number 
-      else
-       
-        Fibonacci(number - 1) + Fibonacci(number - 2) 
-      end
+def fib(n)
+  return 0 if n == 0
+  return 1 if n == 1
+
+  k = fib(n-1) + fib(n-2)
+  return k
 end
 
-fibonacci(3)
+p fib(5)
+#          fib(1)   # |                            |   
+                    # |                            |  
+                    # |    return 0 if 1 == 0      |
+                   #  |    return 1 if 1 == 1      | 1
+                   #  |                            |   
+                    # |----------------------------|
+#          fib(2)   # |                            |   
+                    # |                            |  
+                    # |    return 0 if 2 == 0      |
+                   #  |    return 1 if 2 == 1      | 
+                   #  |                            | 
+                    # |    k = fib(2-1) + fib(2-2) |  1 + 0   /  1 + 0 = 1
+                    # |    return k                |         
+                    # |                            |   
+                    # |----------------------------|
+#          fib(3)   # |                            |   
+                    # |                            |  
+                    # |    return 0 if 3 == 0      |
+                   #  |    return 1 if 3 == 1      | 
+                   #  |                            | 
+                    # |    k = fib(3-1) + fib(3-2) |  2 + 1  / 1 + 1 = 2
+                    # |    return k                |         
+                    # |                            |   
+                    # |----------------------------|
+#          fib(4)   # |                            |   
+                    # |                            |  
+                    # |    return 0 if 4 == 0      |
+                   #  |    return 1 if 4 == 1      | 
+                   #  |                            | 
+                    # |    k = fib(4-1) + fib(4-2) |  3 + 2  / 2 + 1 = 3
+                    # |    return k                |         
+                    # |                            |   
+                    # |----------------------------|
+                    # |                            |   
+#          fib(5)   # |                            |   
+                    # |                            |  
+                    # |    return 0 if 5 == 0      |
+                   #  |    return 1 if 5 == 1      | 
+                   #  |                            | 
+                    # |    k = fib(5-1) + fib(5-2) |  4 + 3  / 3 + 1 = 4
+                    # |    return k                |         
+                    # |                            |   
+                    # |----------------------------|
